012ed8fa57c1c5342d4c33deb14ed7dba294a30b
choukikars16 <sawichoukiker7816@gmail.com>
2023-11-14 16:21:33 +0530

    added image folder in components and edit app.css file and change contact.js file

--- Frontend/src/components/pages/Contact.js
+++ Frontend/src/components/pages/Contact.js
@@ -1,12 +1,34 @@
 import React from 'react'
-
+import contactImage from '../../components/images/contact-img.png';
 const Contact = () => {
   return (
     <div>
-      <h1>Contact</h1>
-      <hr></hr>
+       <h1>Contact</h1>
+      <hr />
+      <h3>How can we help you? Please ask your query here.</h3>
+    <div className="contact-container">
+     
+    <div className="contact-form">
+      <form>
+      <input type="text" placeholder="Your Name" />
+      <input type="email" placeholder="Your Email" />
+      <input type="text" placeholder="Address" />
+      <textarea cols="30" rows="10" placeholder="Write here"></textarea>
+      <div className="btn-box formBtn">
+        <button type="submit" className="btn">
+          Send Message
+        </button>
+      </div>
+    </form>
     </div>
+    <div className="contact-image">
+    <img src={contactImage} alt="Description of the image" />
+    </div>
+  </div>
+</div>
+
   )
 }
 
 export default Contact
+
